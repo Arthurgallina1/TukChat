e67d8f7e12121fb80ea64767f9ec77c8e2d0ae56
Arthur <arthur.gallina@gmail.com>
2020-04-22 23:08:08 -0300

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const socketio = require('socket.io');
-
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,41 +10,44 @@ const io = socketio(server);
 // const adminNamespace = io.of('/admin');
 
 //Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //Client connection
-let connectionList = [];
+let onlineList = [];
+let messages = [];
 
 //default namespace
-io.on('connection', socket => {
+io.on("connection", (socket) => {
+    socket.emit("message", { id: "Server", msg: "Welcome to the chat" });
+    socket.emit("onlineList", () => {
+        onlineList;
+    });
 
-    socket.emit('message', ({id: 'Server', msg: 'Welcome to the chat'}));
-
-    socket.join('room', () => {
-        let rooms = Object.keys(socket.rooms)
-        console.log(rooms)
-    })
-    socket.to('room').emit('event', 'someroom')
+    socket.join("room", () => {
+        let rooms = Object.keys(socket.rooms);
+        // console.debug("Rooms", rooms);
+    });
+    socket.to("room").emit("event", "someroom");
     //broadcast when a user connects
-    socket.on('loginMessage', (id) => {
-        connectionList.push(id)
-        socket.broadcast.emit('userLogin', `${id}`);
-    })
-    
-    socket.on('message', ({id, msg}) => {
-        console.log('received message: '+ id + ' : ' + msg);
-        io.emit('message', ({id, msg}));
-    })
+    socket.on("loginMessage", (id) => {
+        onlineList.push(id);
+        console.debug("Logados: ", onlineList);
+        socket.broadcast.emit("onlineList", onlineList);
+        socket.broadcast.emit("userLogin", `${id}`);
+    });
 
-    socket.on('disconnect', () => {
+    socket.on("message", ({ id, msg }) => {
+        messages.push({ id, msg });
+        console.log("received message: " + id + " : " + msg);
+        io.emit("message", { id, msg });
+    });
+
+    socket.on("disconnect", () => {
         //io.emit pra todos
-        console.log('user disconnected')
-        io.emit('message', 'User Joao has left the chat');
-    })
-    // console.log(connectionList)
-
-})
-
-
+        console.log(`user ${socket.id} has disconnected`);
+        io.emit("message", "User Joao has left the chat");
+    });
+    // console.log(onlineList)
+});
 
 const PORT = 8000 || process.ENV.PORT;
 
--- frontend/src/Components/Main/index.js
+++ frontend/src/Components/Main/index.js
@@ -1,90 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import openSocket from 'socket.io-client';
-import { useParams } from 'react-router-dom'
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import openSocket from "socket.io-client";
+import { useParams } from "react-router-dom";
+import "./styles.css";
 
-const socket = openSocket('http://localhost:8000');
+const socket = openSocket("http://localhost:8000");
 
 export default function Main() {
     const { id } = useParams();
-    const [ usersOnline, setUsersOnline ] = useState([]);
-    const [msg, setMsg] = useState('');
-    const [ chat, setChat ] = useState([]);
+    const [usersOnline, setUsersOnline] = useState([]);
+    const [msg, setMsg] = useState("");
+    const [chat, setChat] = useState([]);
 
     useEffect(() => {
-        socket.on('connect', function(){
+        socket.on("connect", function () {
             // socket.join
-            socket.emit('loginMessage', `${id}`);
+            socket.emit("loginMessage", `${id}`);
 
-            socket.on('event', () => {
-                console.log('entrei na room ')
-            })
+            // socket.on("event", () => {
+            //     console.log("entrei na room ");
+            // });
+            socket.on("onlineList", (userlist) => {
+                setUsersOnline([userlist]);
+                console.debug("User List", userlist);
+            });
+
+            socket.on("userLogin", (user) => {
+                console.debug("userLogin", `user ${user} has logged`);
+                // setUsersOnline(...usersOnline, id);
+                setUsersOnline([...usersOnline, user]);
+            });
+
+            socket.on("message", ({ msg, id }) => {
+                setChat([...chat, { msg, id }]);
+            });
         });
 
-       
-                //broadcast when a user connects
-                // socket.broadcast.emit() //broadcast emit pra todo mundo menos que esta
-    
-    }, [])
+        //broadcast when a user connects
+        // socket.broadcast.emit() //broadcast emit pra todo mundo menos que esta
+    }, []);
 
-    useEffect(() => {
-        socket.on('message', ({msg, id}) => {
-            // console.log('msg recebida ' + msg )
-            setChat([...chat, {msg, id}])
-        })
+    // useEffect(() => {
+    //     socket.on("message", ({ msg, id }) => {
+    //         // console.log('msg recebida ' + msg )
+    //         setChat([...chat, { msg, id }]);
+    //     });
+    // }, [chat]);
 
-    }, [chat])
+    // useEffect(() => {
+    //     socket.on("userLogin", (user) => {
+    //         setUsersOnline([...usersOnline, user]);
+    //     });
+    // }, [usersOnline]);
 
-    useEffect(() => {
-        socket.on('userLogin', (user) => {
-            setUsersOnline([...usersOnline, user])
-        });
-    }, [usersOnline])
-
-    function handleSubmit(){
-        socket.emit('message', ({id, msg}));
-        setMsg('');
+    function handleSubmit() {
+        socket.emit("message", { id, msg });
+        setMsg("");
     }
-
 
     return (
         <div>
-            <div className="main">
+            <div className='main'>
                 <h3>SocketChat</h3>
                 <h4>Welcome, {id}!</h4>
-                <div className="chat-wrapper">
-                    <div className="online-section">
+                <div className='chat-wrapper'>
+                    <div className='online-section'>
                         <ul>
                             <p>ONLINE NOW</p>
                             <li>{id}</li>
-                            {
-                                usersOnline.map(user => (
-                                    <li key={user}>{user}</li>
-                                ))
-                            }
+                            {usersOnline.map((user) => (
+                                <li key={user}>{user}</li>
+                            ))}
                         </ul>
                     </div>
-                    <div className="chat-section">
-                        <div className="chat-box">
-                            {
-                                chat.map(msg => (
-                                <p key={`${msg.id}${msg.msg}`}><span>{msg.id}</span><small>{msg.msg}</small></p>
-                                ))
-                            }
-
+                    <div className='chat-section'>
+                        <div className='chat-box'>
+                            {chat.map((msg) => {
+                                return (
+                                    <p key={`${msg.id}${msg.msg}`}>
+                                        <span>{msg.id}</span>
+                                        <small>{msg.msg}</small>
+                                    </p>
+                                );
+                            })}
                         </div>
-                        <div className="form-section">
-                            <input type="text" value={msg} onChange={(e)=> setMsg(e.target.value)}/>
+                        <div className='form-section'>
+                            <input
+                                type='text'
+                                value={msg}
+                                onChange={(e) => setMsg(e.target.value)}
+                            />
                             <button onClick={() => handleSubmit()}>SEND</button>
                         </div>
                     </div>
-                    <div className="group-section">
+                    <div className='group-section'>
                         <p>ROOM</p>
-
                     </div>
                 </div>
-                
             </div>
         </div>
-    )
+    );
 }
