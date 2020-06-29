4de7c3281ced01d625e42e11d7e6649acd16f7e9
Arthurgallina1 <arthur.gallina@gmail.com>
2020-06-29 16:35:51 -0300

    add gif and splash

--- frontend/src/Components/Chat/index.js
+++ frontend/src/Components/Chat/index.js
@@ -6,92 +6,88 @@ import MessageBox from "../MessageBox";
 import Input from "../Input";
 import InfoBar from "../InfoBar";
 import Sidebar from "../SideBar";
-const socket = openSocket("https://tu-socket-chat.herokuapp.com");
+const socket = openSocket("http://localhost:8000");
 
 export default function Chat() {
-    const { id, room } = useParams();
-    const [usersOnline, setUsersOnline] = useState([]);
-    const [msg, setMsg] = useState("");
-    const [activeRooms, setActiveRooms] = useState([]);
-    const [messages, setMessages] = useState([]);
+  const { id, room } = useParams();
+  const [usersOnline, setUsersOnline] = useState([]);
+  const [msg, setMsg] = useState("");
+  const [activeRooms, setActiveRooms] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        socket.on("connect", function () {
-            // socket.join
-            let connectionObj = {
-                username: id,
-                room,
-            };
-            socket.emit("newuser", connectionObj);
-        });
-    }, []);
+  useEffect(() => {
+    socket.on("connect", function () {
+      // socket.join
+      let connectionObj = {
+        username: id,
+        room,
+      };
+      socket.emit("newuser", connectionObj);
+    });
+  }, []);
 
-    useEffect(() => {
-        socket.on("message", (msg) => {
-            // console.log('msg recebida ' + msg )
-            setMessages([...messages, msg]);
-        });
-    }, [messages]);
+  useEffect(() => {
+    socket.on("message", (msg) => {
+      // console.log('msg recebida ' + msg )
+      setMessages([...messages, msg]);
+    });
+  }, [messages]);
 
-    useEffect(() => {
-        socket.on("rooms", (rooms) => {
-            // console.log('msg recebida ' + msg )
-            setActiveRooms([...rooms]);
-        });
-    }, [messages]);
+  useEffect(() => {
+    socket.on("rooms", (rooms) => {
+      // console.log('msg recebida ' + msg )
+      setActiveRooms([...rooms]);
+    });
+  }, [messages]);
 
-    useEffect(() => {
-        socket.on("private message", (msg) => {
-            setMessages([...messages, msg]);
-        });
-    }, [messages]);
+  useEffect(() => {
+    socket.on("private message", (msg) => {
+      setMessages([...messages, msg]);
+    });
+  }, [messages]);
 
-    useEffect(() => {
-        socket.on("users", (users) => {
-            // setUsersOnline([...usersOnline, user]);
-            setUsersOnline([...users]);
-        });
-    }, [usersOnline]);
+  useEffect(() => {
+    socket.on("users", (users) => {
+      // setUsersOnline([...usersOnline, user]);
+      setUsersOnline([...users]);
+    });
+  }, [usersOnline]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        socket.emit("message", { id, msg, room });
-        setMsg("");
-    };
-    function handlePoke(user) {
-        const socketid = user.id;
-        if (user.username !== id) {
-            socket.emit("poke", { socketid, id });
-        }
+    socket.emit("message", { id, msg, room });
+    setMsg("");
+  };
+  function handlePoke(user) {
+    const socketid = user.id;
+    if (user.username !== id) {
+      socket.emit("poke", { socketid, id });
     }
+  }
 
-    function changeRoom(room) {
-        console.log(room);
-    }
+  function changeRoom(room) {
+    console.log(room);
+  }
 
-    return (
-        <div className='chat-container'>
-            {/* <h3>Welcome</h3> */}
-            <div className='outerContainer'>
-                <Sidebar
-                    id={id}
-                    usersOnline={usersOnline}
-                    handlePoke={handlePoke}
-                    room={room}
-                    activeRooms={activeRooms}
-                />
-                <div className='container'>
-                    <InfoBar room={room} />
-                    <MessageBox messages={messages} handlePoke={handlePoke} />
-                    <Input
-                        message={msg}
-                        setMessage={setMsg}
-                        sendMessage={handleSubmit}
-                    />
-                </div>
-                {/* <TextContainer users={users}/> */}
-            </div>
+  return (
+    <div className="chat-container">
+      {/* <h3>Welcome</h3> */}
+      <div className="outerContainer">
+        <Sidebar
+          id={id}
+          usersOnline={usersOnline}
+          handlePoke={handlePoke}
+          room={room}
+          activeRooms={activeRooms}
+        />
+        <div className="container">
+          <InfoBar room={room} />
+          <MessageBox messages={messages} handlePoke={handlePoke} />
+          <Input message={msg} setMessage={setMsg} sendMessage={handleSubmit} />
         </div>
-    );
+        {/* <TextContainer users={users}/> */}
+      </div>
+    </div>
+  );
 }
